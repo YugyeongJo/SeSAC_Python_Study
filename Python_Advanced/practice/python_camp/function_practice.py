02e439fb21d0de642d60ec570aa2d8a819df7ed3
YugyeongJo <yugyeongjo1215@gamil.com>
2024-08-07 17:57:39 +0900

    update

--- Python_Advanced/practice/python_camp/function_practice.py
+++ Python_Advanced/practice/python_camp/function_practice.py
@@ -487,3 +487,28 @@ def cache_to_pickle(function):
 
 # f(double)(3)
 
+# =================================== #
+# 강사님 code
+
+def cache_to_pickle2(function):
+    
+    def f(x):
+        pickle_path = 'pickle_path'
+        file_path = f'{pickle_path}/{x}.pickle'
+        
+        if not os.path.exists(pickle_path):
+            os.makedirs(pickle_path)
+            
+        if not os.path.exists(file_path):
+            res = function(x)
+            pickle.dump(res, open(file_path, 'wb+'))
+            return res
+        else:
+            res = pickle.load(open(file_path, 'rb'))
+            return res
+    return f
+
+def double(x):
+    return 2*x
+
+print(cache_to_pickle2(double)(2))
