454351cabbde7dcc377b37cc9a9f1b303569f11e
YugyeongJo <yugyeongjo1215@gamil.com>
2024-08-29 11:01:29 +0900

    ADT stack complete

--- Python_Advanced/practice/algorithm-in-python-master/skeleton/ADT/stack.py
+++ Python_Advanced/practice/algorithm-in-python-master/skeleton/ADT/stack.py
@@ -13,37 +13,58 @@ class Stack:
         if self.backend == list:
             self.list = list(elements)
         elif self.backend == LinkedList:
-            self.linked_list = None # make right linked list 
+            self.linked_list = LinkedList(list(elements))
     
+    def elements(self):
+        if self.backend == list:
+            return self.list
+        elif self.backend == LinkedList:
+            return self.linked_list
+        
     def push(self, elem):
         if self.backend == list:
             self.list.append(elem)
+        elif self.backend == LinkedList:
+            return self.linked_list.append_to_tail()
 
     def pop(self):
         if self.backend == list:
             return self.list.pop()
+        elif self.backend == LinkedList:
+            return self.linked_list.pop_from_head()
 
     def top(self):
         if self.backend == list:
             return self.list[-1]
+        elif self.backend == LinkedList:
+            return self.linked_list.elements()[-1]
 
     def is_empty(self):
-        if self.backend == list:
-            return self.list == []
+        return len(self.elements()) == 0
+        # if self.backend == list:
+        #     return self.list == []
+        # elif self.backend == LinkedList:
+        #     return self.linked_list
 
     def size(self):
         if self.backend == list:
             return len(self.list)
+        elif self.backend == LinkedList:
+            return len(self.linked_list.elements())
+        
+    def __str__(self):
+        return str(self.elements())
 
 if __name__ == '__main__':
-    available_backends = [list, LinkedList, DoublyLinkedList]
-    available_backends = [list]
+    # available_backends = [list, LinkedList, DoublyLinkedList]
+    available_backends = [LinkedList]
 
     for backend in available_backends:
         s1 = Stack(3,2,1,4)
         assert s1.top() == 4 
         assert not s1.is_empty()
-        assert s1.pop() == 4 
+        assert s1.pop() == 4
+        
         assert s1.top() == 1 
         
         s1.push(5) 
